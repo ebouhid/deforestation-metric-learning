dd710745e7a8ad76ddfecc0ccf5626b2311f6052
ebouhid <ebneto@unifesp.br>
2025-01-14 16:17:44 -0300

    Evaluate multiple miner types. Also included train and validation samplers.

--- metric_learning.py
+++ metric_learning.py
@@ -11,7 +11,6 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
-torch.set_float32_matmul_precision('medium')
 
 def validate(model, val_loader, loss_func, device):
         model.eval()
@@ -61,27 +60,38 @@ def train(model, loss_func, mining_func, device, train_loader, optimizer, epoch)
 if __name__ == "__main__":
     for backbone_name in ["resnet18", "resnet50", "resnet101"]:
         os.makedirs(os.path.join("dml_results", backbone_name), exist_ok=True)
-        for i in range(1, 4):
+        os.makedirs(os.path.join("dml_results", backbone_name, "models"), exist_ok=True)
+        for miner_type in ["semihard", "hard", "all"]:
             # Set up dataset
             root_dir = "./segment_embeddings_classification_dataset_norsz/"
             train_regions = ["x02", "x06", "x07", "x09", "x10"]
             val_regions = ["x03", "x08"]
             test_regions = ["x01", "x04"]
             transform = True
+            save_every_epoch = True
 
             train_dataset = SegmentClassificationDataset(
-                root_dir=root_dir, regions=train_regions, transform=transform, return_dict=False)
+                root_dir=root_dir, regions=train_regions, transform=transform, return_dict=False, exc_labels=[-1])
             val_dataset = SegmentClassificationDataset(
-                root_dir=root_dir, regions=val_regions, transform=False, return_dict=False)
+                root_dir=root_dir, regions=val_regions, transform=False, return_dict=False, exc_labels=[-1])
             
             print(f"Len Train Dataset: {len(train_dataset)} | Len Val Dataset: {len(val_dataset)}")
+            print(50 * '=')
+            print(f"Forest samples in train dataset: {sum([1 for x in train_dataset if x[1] == 0])}")
+            print(f"Recent deforestation samples in train dataset: {sum([1 for x in train_dataset if x[1] == 1])}")
+            print(50 * "-")
+            print(f"Forest samples in val dataset: {sum([1 for x in val_dataset if x[1] == 0])}")
+            print(f"Recent deforestation samples in val dataset: {sum([1 for x in val_dataset if x[1] == 1])}")
 
             # DataLoader with Sampler
             batch_size = 256
+
+            train_sampler = MPerClassSampler(labels=train_dataset.labels, m=batch_size//2, length_before_new_iter=len(train_dataset))
+            val_sampler = MPerClassSampler(labels=val_dataset.labels, m=batch_size//2, length_before_new_iter=len(val_dataset))
             
             train_loader = DataLoader(
-                train_dataset, batch_size=batch_size, num_workers=16)
-            val_loader = DataLoader(val_dataset, batch_size=batch_size, num_workers=4, shuffle=False)
+                train_dataset, batch_size=batch_size, num_workers=32, sampler=train_sampler)
+            val_loader = DataLoader(val_dataset, batch_size=batch_size, num_workers=4, shuffle=False, sampler=val_sampler)
 
             # Set up embedding model
             class TrunkModel(torch.nn.Module):
@@ -112,7 +122,7 @@ if __name__ == "__main__":
 
             # Loss and Miner
             loss = TripletMarginLoss(margin=0.2)
-            miner = TripletMarginMiner(margin=0.2, type_of_triplets="hard")
+            miner = TripletMarginMiner(margin=0.2, type_of_triplets=miner_type)
 
             # Optimizer
             optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
@@ -121,12 +131,13 @@ if __name__ == "__main__":
             print(f"Initial Validation Mean Loss: {initial_val_loss:.4f}")
 
             # Train
-            num_epochs = 100
+            num_epochs = 50
             val_data = []
             train_data = []
             best_val_loss = float("inf")
             for epoch in range(1, num_epochs + 1):
-                print(f"Epoch {epoch}/{num_epochs}")
+
+                print(f"Epoch {epoch}/{num_epochs} | Miner: {miner.type_of_triplets}")
                 train_mean_loss = train(model, loss, miner, device, train_loader, optimizer, epoch)
                 train_data.append({"epoch": epoch, "loss": train_mean_loss})
                 val_loss = validate(model, val_loader, loss, device)
@@ -134,8 +145,10 @@ if __name__ == "__main__":
                 print(f"Validation Mean Loss on epoch {epoch}: {val_loss:.4f}")
                 if val_loss < best_val_loss:
                     best_val_loss = val_loss
-                    torch.save(model.state_dict(), os.path.join("dml_results", backbone_name, f"best_model_state_dict_{i}.pth"))
+                    torch.save(model.state_dict(), os.path.join("dml_results", backbone_name, "models", f"best_model_state_dict_{miner_type}_epoch-{epoch}.pth"))
                     print("New best model saved!")
+                if save_every_epoch:
+                    torch.save(model.state_dict(), os.path.join("dml_results", backbone_name, "models", f"model_state_dict_{miner_type}_epoch-{epoch}.pth"))
             print("Training complete")
 
             # Validation
@@ -149,14 +162,14 @@ if __name__ == "__main__":
             # Plot train and val losses
             plt.figure(figsize=(12, 6))
             plt.plot(training_info["epoch"], training_info["loss"], label="Training Loss")
-            plt.savefig(os.path.join("dml_results", backbone_name, f"training_loss{i}.png"))
+            plt.savefig(os.path.join("dml_results", backbone_name, f"training_loss{miner_type}.png"))
 
             plt.clf()
             plt.figure(figsize=(12, 6))
             plt.plot(val_info["epoch"], val_info["val_loss"], label="Validation Loss")
-            plt.savefig(os.path.join("dml_results", backbone_name, f"validation_loss{i}.png"))
+            plt.savefig(os.path.join("dml_results", backbone_name, f"validation_loss{miner_type}.png"))
 
             # Save to .txt
-            with open(os.path.join("dml_results", backbone_name, f"validation_loss{i}.txt"), "w") as f:
+            with open(os.path.join("dml_results", backbone_name, f"validation_loss{miner_type}.txt"), "w") as f:
                 f.write(f"Initial Validation Mean Loss: {initial_val_loss:.4f}\n")
                 f.write(f"Final Validation Mean Loss: {final_val_loss:.4f}\n")   
