dcea057764e3e3b29bbae2a0af2e0fd3ee659166
ebouhid <ebneto@unifesp.br>
2025-01-14 16:18:18 -0300

    Support for label exclusion (necessary if we only want to get recent_def).

--- dataset.py
+++ dataset.py
@@ -5,7 +5,7 @@ import torchvision.transforms as T
 import torch
 
 class SegmentClassificationDataset(Dataset):
-    def __init__(self, root_dir, regions, transform=False, return_dict=True):
+    def __init__(self, root_dir, regions, transform=False, return_dict=True, exc_labels=None):
         self.root_dir = root_dir
         self.regions = regions
         self.transform = transform
@@ -15,6 +15,21 @@ class SegmentClassificationDataset(Dataset):
         
         # Populate image paths and labels based on specified regions
         self._load_images()
+
+        # Remove examples with labels in exc_labels
+        if exc_labels:
+            self._remove_examples(exc_labels)
+    
+    def _remove_examples(self, exc_labels):
+        """Helper function to remove examples with labels in exc_labels."""
+        new_image_paths = []
+        new_labels = []
+        for i in range(len(self.image_paths)):
+            if self.labels[i] not in exc_labels:
+                new_image_paths.append(self.image_paths[i])
+                new_labels.append(self.labels[i])
+        self.image_paths = new_image_paths
+        self.labels = new_labels
 
     def _load_images(self):
         """Helper function to load image paths and labels based on regions and transform setting."""
@@ -39,7 +54,7 @@ class SegmentClassificationDataset(Dataset):
                     self.image_paths.append(file_path)
                     
                     label = filename.split('_')[1].split('.')[0]
-                    label = 0 if label == 'forest' else 1 if 'recent_def' in label else -1
+                    label = 0 if label == 'forest' else 1 if 'recent-def' in label else -1
                     self.labels.append(label)
 
     def __len__(self):
